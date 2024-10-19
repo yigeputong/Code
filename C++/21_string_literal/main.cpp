5352a8a7e4a7cfd97bdf00580a89a1db2c452913
ygpt-win-1 <yigeputong@outlook.com>
2024-10-19 20:20:11 +0800

    22_const

--- C++/21_string_literal/main.cpp
+++ C++/21_string_literal/main.cpp
@@ -11,11 +11,11 @@ int main()
     const char name[12] = "yige\0putong";  //(2)变成了 (const char [12])"yige\000putong"
     cout << strlen(name) << endl;   //(3)输出"4"，因为 strlen() 函数只计算到第一个终止符
 
-    const char* name1 = "yigeputong";
-    const wchar_t* name2 = L"yigeputong";    //(4)这是宽字符，要在字符串前面加上 "L"
-    const char* name3 = u8"yigeputong";      //(7)这是 8 位字符，要在字符串前面加上 "u8"
-    const char16_t* name4 = u"yigeputong";   //(5)这是 16 位字符，要在字符串前面加上 "u"
-    const char32_t* name5 = U"yigeputong";   //(6)这是 32 位字符，要在字符串前面加上 "U"
+    const char name1[] = "yigeputong";
+    const wchar_t name2[] = L"yigeputong";    //(4)这是宽字符，要在字符串前面加上 "L"
+    const char name3[] = u8"yigeputong";      //(5)这是 8 位字符，要在字符串前面加上 "u8"
+    const char16_t name4[] = u"yigeputong";   //(6)这是 16 位字符，要在字符串前面加上 "u"
+    const char32_t name5[] = U"yigeputong";   //(7)这是 32 位字符，要在字符串前面加上 "U"
 
     cout << "Press Enter to continue... ";
     cin.get();
